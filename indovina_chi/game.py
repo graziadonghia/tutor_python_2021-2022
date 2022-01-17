c70ef4f7ed5705fd71f5fc22bcd46fc16a85e184
Grazia D'Onghia <s284633@studenti.polito.it>
2022-01-17 01:00:18 +0100

    solved indovina chi

--- indovina_chi/game.py
+++ indovina_chi/game.py
@@ -1,4 +1,35 @@
 from characters import printCharacter
-def play(filename, characters):
+
+def matchOk(character, questions):
+    for question in questions:
+        prop = question[0]
+        value = question[1]
+        if character[prop] != value:
+            return False
+    return True
+
+def play(filename, characters, properties):
     questions = [] #list of questions
+    move = 0 #moves counter
     f = open(filename, "r")
+    for lineStr in f:
+        move += 1
+        print("Mossa "+str(move)+" - domanda: "+lineStr)
+        selected = 0 #selected characters
+        line = lineStr.replace("\r\n", "").split("=")
+        questions.append(list(line))
+        print("Personaggi selezionati:")
+        selected = 0
+        for character in characters:
+            if matchOk(character, questions):
+                selected += 1
+                winner = dict(character) #possible winner
+                printCharacter(character, properties)
+        print("\n")
+    f.close()
+    print("\n")
+    if selected == 1:
+        print("Gioco terminato, hai vinto! E' stato selezionato:")
+        printCharacter(winner, properties)
+    else:
+          print("Peccato, hai perso :-(")
--- indovina_chi/main.py
+++ indovina_chi/main.py
@@ -1,10 +1,14 @@
 CHARACTERS_FILENAME = "personaggi.txt"
 QUESTIONS_FILENAME = "domande1.txt"
 from characters import printCharacter, saveCharacters
+from game import play
 def main():
     characters, properties = saveCharacters(CHARACTERS_FILENAME)
     print("Personaggi del gioco:")
     for c in characters:
         printCharacter(c, properties[1:])
+    print("\n\n")
+    play(QUESTIONS_FILENAME, characters, properties)
+    
 
 main()
